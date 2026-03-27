03bf916c4388d78dbe9d4b7ed90b9b022c8ceb3c
Chahat Joshi <sdhrtjoshi@gmail.com>
2026-03-27 19:53:56 +0530

    update api url

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -11,7 +11,7 @@ const User = () => {
         const fetchData = async()=>{
             try {
 
-                const response = await axios.get("http://localhost:8000/api/users")
+                const response = await axios.get("https://data-stack-server-zb9q.onrender.com/api/users")
                 setUsers(response.data)
 
             } catch (error) {
@@ -22,7 +22,7 @@ const User = () => {
     },[]);
 
     const deleteUser = async (userId) =>{
-        await axios.delete(`http://localhost:8000/api/delete/user/${userId}`)
+        await axios.delete(`https://data-stack-server-zb9q.onrender.com/api/delete/user/${userId}`)
         .then((response)=>{
             setUsers((prevUser) =>prevUser.filter((user)=>user._id !==userId))
             toast
