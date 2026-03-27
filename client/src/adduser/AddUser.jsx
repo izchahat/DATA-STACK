cc147f549e10442556e4e30ff300366b205668ff
Chahat Joshi <sdhrtjoshi@gmail.com>
2026-03-27 19:29:49 +0530

    re-add client folder properly

---
+++ client/src/adduser/AddUser.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react"
+import "./adduser.css"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const AddUser = () => {
+    const users = {
+        name: "",
+        email: "",
+        address: "",
+    };
+    const [user, setUser,] = useState(users);
+    const navigate = useNavigate();
+
+    const inputHandler = (e) =>{
+        const {name, value}= e.target
+        console.log(name, value)
+        
+        setUser({ ...user, [name]: value });
+    }
+
+    const SubmitForm = async(e)=>{
+        e.preventDefault();
+        await axios.post("http://localhost:8000/api/user",user)
+        .then((response)=>{
+            toast.success(response.data.message, { position: "top-right" });
+            navigate("/")
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+
+
+
+  return (
+    <div className="addUser">
+    <Link to="/" type="button" class="btn btn-secondary">
+    <i class="fa-solid fa-backward"></i> Back
+    </Link>
+    <h3>Add New User</h3>
+    <form className="addUserForm" onSubmit={SubmitForm}>
+        <div className="inputGroup">
+        <lable htmlFor="name">Name:</lable>
+        <input
+        type="text"
+        id="name"
+        onChange={inputHandler}
+        name="name"
+        autoComplete="off"
+        placeholder="Enter your Name"
+        />
+
+        </div>
+        <div className="inputGroup">
+        <lable htmlFor="email">Email:</lable>
+        <input
+        type="email"
+        id="email"
+        onChange={inputHandler}
+        name="email"
+        autoComplete="off"
+        placeholder="Enter your Email"
+        />
+
+        </div>
+        <div className="inputGroup">
+        <lable htmlFor="address">Address:</lable>
+        <input
+        type="text"
+        id="address"
+        onChange={inputHandler}
+        name="address"
+        autoComplete="off"
+        placeholder="Enter your Address"
+        />
+
+        </div>
+        <div className="inputGroup">
+            <button type="submit" class="btn btn-primary">
+            Submit
+            </button>
+        </div>
+
+
+    </form>
+    </div>
+  )
+}
+
+export default AddUser
